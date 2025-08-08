77510bd0cf50b04b82a7baa00dc78874a484b3c1
Георгий Аникин <ubilby@MacBook-Air-Georgij.local>
2025-08-08 17:56:23 +0300

    fix by flake8

--- main.py
+++ main.py
@@ -8,15 +8,13 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-
 from dotenv import load_dotenv
-
 
 # Загрузка переменных из .env файла
 load_dotenv()
 
 # Bot token can be obtained via https://t.me/BotFather
-TOKEN = getenv("BOT_TOKEN") 
+TOKEN = getenv("BOT_TOKEN")
 
 # All handlers should be attached to the Router (or Dispatcher)
 
@@ -28,13 +26,18 @@ async def command_start_handler(message: Message) -> None:
     """
     This handler receives messages with `/start` command
     """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # Most event objects have aliases for API methods that
+    # can be called in events' context
+    # For example if you want to answer to incoming message
+    # you can use `message.answer(...)` alias
+    # and the target chat will be passed to
+    # :ref:`aiogram.methods.send_message.SendMessage`
     # method automatically or call API method directly via
     # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!") # type: ignore 
+
+    await message.answer(
+        f"Hello, {html.bold(message.from_user.full_name)}!"  # type: ignore
+    )
 
 
 @dp.message()
@@ -42,7 +45,8 @@ async def echo_handler(message: Message) -> None:
     """
     Handler will forward receive a message back to the sender
 
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    By default, message handler will handle all message types
+    (like a text, photo, sticker etc.)
     """
     try:
         # Send a copy of the received message
@@ -53,8 +57,12 @@ async def echo_handler(message: Message) -> None:
 
 
 async def main() -> None:
-    # Initialize Bot instance with default bot properties which will be passed to all API calls
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML)) # type: ignore 
+    # Initialize Bot instance with default bot properties
+    # which will be passed to all API calls
+    bot = Bot(
+        token=TOKEN,  # type: ignore
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
 
     # And the run events dispatching
     await dp.start_polling(bot)
